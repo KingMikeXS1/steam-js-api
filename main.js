04ac929897687ef01227365c3f758540272d6d24
KingMikeXS1 <78938662+KingMikeXS1@users.noreply.github.com>
2021-04-17 16:44:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const api = require('steam-js-api')
-api.setKey('{YOUR_KEY_HERE}')
+api.setKey('{6B328D41EE66949204BBCEBA81C3852A}')
 
-steamID = '6B328D41EE66949204BBCEBA81C3852A' // My Steam ID, feel free to use it for testing :)
-appID = 730 // We only want to check for one game
+steamID = ''
+appID = []
 moreInfo = true // Provide more info (name, links)
 
 // With a callback
-api.getOwnedGames(steamID, appID, moreInfo, (result) => {
-    if (result.error)
-        console.error(result.error)
-    else
-        console.log(result.data.games[0])
-})
+api.getOwnedGames(steamID, appID, moreInfo).then(result => {
+    console.log(result.data)
+}).catch(console.error)
